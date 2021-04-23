76ecf1ddc0053e5257e77f64ec048c0935a1d881
ShchukinNY <ShchukinNY@exmaple.com>
2021-04-23 22:36:00 +0300

    code: добавлена функция make_histogram

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@ input_numbers(size_t count) {
     }
     return result;
 }
+
+
+
 void find_minmax(vector<double> numbers, double& min, double& max) {
     min = numbers[0];
     max = numbers[0];
@@ -23,7 +26,27 @@ void find_minmax(vector<double> numbers, double& min, double& max) {
         }
     }
         return ;
+}
+
+
+
+vector <size_t> make_histogram(vector<double> numbers,size_t bin_count)
+    {
+        double max;
+        double min;
+        find_minmax(numbers, min, max);
+         vector<size_t> bins(bin_count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
+        }
+        bins[bin]++;
     }
+    return bins;
+}
+
+
 
 int
 main() {
@@ -38,25 +61,8 @@ main() {
     cin >> bin_count;
 
     // Обработка данных
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
+     const auto bins = make_histogram(numbers, bin_count);
 
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        bins[bin]++;
-    }
 
     // Вывод данных
     const size_t SCREEN_WIDTH = 80;

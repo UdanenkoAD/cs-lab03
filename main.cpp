f12ad32055e5bb982c837c66196e96c42a5eef72
SchcukinNY <SchcukinNY@exmaple.com>
2021-05-02 03:19:29 +0300

    code: добавлен модульный тест для функции find_minmax и файлы к ним

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -13,20 +14,6 @@ input_numbers(size_t count){
 }
 
 
-
-void find_minmax(vector<double> numbers, double& min, double& max){
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
-        return ;
-}
 
 
 
